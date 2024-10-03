e9bb6cdf32f16b2bb822dc1b872ad5cb0bf84d9a
NicoPinoVergara <nicolas.pinovergara@gmail.com>
2024-10-03 19:40:55 -0300

    Agregar archivos de proyecto.

---
+++ Eva2 Proyecto NET/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tbxApellidos = New TextBox()
+        tbxNombres = New TextBox()
+        tbxRut = New TextBox()
+        lbApellidos = New Label()
+        lbNombres = New Label()
+        lbRut = New Label()
+        Panel1 = New Panel()
+        lbSexo = New Label()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        lbComuna = New Label()
+        lbCiudad = New Label()
+        lbObservacion = New Label()
+        tbxCiudad = New TextBox()
+        tbxObservacion = New TextBox()
+        btnGuardarMySQL = New Button()
+        btnBuscar = New Button()
+        cmbComuna = New ComboBox()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' tbxApellidos
+        ' 
+        tbxApellidos.Location = New Point(125, 137)
+        tbxApellidos.Name = "tbxApellidos"
+        tbxApellidos.Size = New Size(376, 27)
+        tbxApellidos.TabIndex = 14
+        ' 
+        ' tbxNombres
+        ' 
+        tbxNombres.Location = New Point(125, 92)
+        tbxNombres.Name = "tbxNombres"
+        tbxNombres.Size = New Size(376, 27)
+        tbxNombres.TabIndex = 13
+        ' 
+        ' tbxRut
+        ' 
+        tbxRut.Location = New Point(125, 45)
+        tbxRut.Name = "tbxRut"
+        tbxRut.Size = New Size(202, 27)
+        tbxRut.TabIndex = 12
+        ' 
+        ' lbApellidos
+        ' 
+        lbApellidos.AutoSize = True
+        lbApellidos.Location = New Point(43, 137)
+        lbApellidos.Name = "lbApellidos"
+        lbApellidos.Size = New Size(72, 20)
+        lbApellidos.TabIndex = 11
+        lbApellidos.Text = "Apellidos"
+        ' 
+        ' lbNombres
+        ' 
+        lbNombres.AutoSize = True
+        lbNombres.Location = New Point(43, 92)
+        lbNombres.Name = "lbNombres"
+        lbNombres.Size = New Size(70, 20)
+        lbNombres.TabIndex = 10
+        lbNombres.Text = "Nombres"
+        ' 
+        ' lbRut
+        ' 
+        lbRut.AutoSize = True
+        lbRut.Location = New Point(43, 45)
+        lbRut.Name = "lbRut"
+        lbRut.Size = New Size(31, 20)
+        lbRut.TabIndex = 9
+        lbRut.Text = "Rut"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(lbSexo)
+        Panel1.Controls.Add(RadioButton3)
+        Panel1.Controls.Add(RadioButton2)
+        Panel1.Controls.Add(RadioButton1)
+        Panel1.Location = New Point(43, 198)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(458, 114)
+        Panel1.TabIndex = 15
+        ' 
+        ' lbSexo
+        ' 
+        lbSexo.AutoSize = True
+        lbSexo.Location = New Point(24, 9)
+        lbSexo.Name = "lbSexo"
+        lbSexo.Size = New Size(41, 20)
+        lbSexo.TabIndex = 21
+        lbSexo.Text = "Sexo"
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(285, 58)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(120, 24)
+        RadioButton3.TabIndex = 20
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "No especifica"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(173, 58)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(68, 24)
+        RadioButton2.TabIndex = 19
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Mujer"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(24, 58)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(85, 24)
+        RadioButton1.TabIndex = 18
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Hombre"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' lbComuna
+        ' 
+        lbComuna.AutoSize = True
+        lbComuna.Location = New Point(43, 366)
+        lbComuna.Name = "lbComuna"
+        lbComuna.Size = New Size(64, 20)
+        lbComuna.TabIndex = 16
+        lbComuna.Text = "Comuna"
+        ' 
+        ' lbCiudad
+        ' 
+        lbCiudad.AutoSize = True
+        lbCiudad.Location = New Point(43, 418)
+        lbCiudad.Name = "lbCiudad"
+        lbCiudad.Size = New Size(56, 20)
+        lbCiudad.TabIndex = 17
+        lbCiudad.Text = "Ciudad"
+        ' 
+        ' lbObservacion
+        ' 
+        lbObservacion.AutoSize = True
+        lbObservacion.Location = New Point(43, 467)
+        lbObservacion.Name = "lbObservacion"
+        lbObservacion.Size = New Size(91, 20)
+        lbObservacion.TabIndex = 18
+        lbObservacion.Text = "Observacion"
+        ' 
+        ' tbxCiudad
+        ' 
+        tbxCiudad.Location = New Point(159, 418)
+        tbxCiudad.Name = "tbxCiudad"
+        tbxCiudad.Size = New Size(342, 27)
+        tbxCiudad.TabIndex = 20
+        ' 
+        ' tbxObservacion
+        ' 
+        tbxObservacion.Location = New Point(159, 467)
+        tbxObservacion.Name = "tbxObservacion"
+        tbxObservacion.Size = New Size(342, 27)
+        tbxObservacion.TabIndex = 21
+        ' 
+        ' btnGuardarMySQL
+        ' 
+        btnGuardarMySQL.Location = New Point(186, 524)
+        btnGuardarMySQL.Name = "btnGuardarMySQL"
+        btnGuardarMySQL.Size = New Size(153, 56)
+        btnGuardarMySQL.TabIndex = 22
+        btnGuardarMySQL.Text = "Guardar My SQL"
+        btnGuardarMySQL.UseVisualStyleBackColor = True
+        ' 
+        ' btnBuscar
+        ' 
+        btnBuscar.Location = New Point(354, 45)
+        btnBuscar.Name = "btnBuscar"
+        btnBuscar.Size = New Size(147, 29)
+        btnBuscar.TabIndex = 23
+        btnBuscar.Text = "Buscar"
+        btnBuscar.UseVisualStyleBackColor = True
+        ' 
+        ' cmbComuna
+        ' 
+        cmbComuna.FormattingEnabled = True
+        cmbComuna.Location = New Point(159, 366)
+        cmbComuna.Name = "cmbComuna"
+        cmbComuna.Size = New Size(342, 28)
+        cmbComuna.TabIndex = 24
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(585, 606)
+        Controls.Add(cmbComuna)
+        Controls.Add(btnBuscar)
+        Controls.Add(btnGuardarMySQL)
+        Controls.Add(tbxObservacion)
+        Controls.Add(tbxCiudad)
+        Controls.Add(lbObservacion)
+        Controls.Add(lbCiudad)
+        Controls.Add(lbComuna)
+        Controls.Add(Panel1)
+        Controls.Add(tbxApellidos)
+        Controls.Add(tbxNombres)
+        Controls.Add(tbxRut)
+        Controls.Add(lbApellidos)
+        Controls.Add(lbNombres)
+        Controls.Add(lbRut)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents tbxApellidos As TextBox
+    Friend WithEvents tbxNombres As TextBox
+    Friend WithEvents tbxRut As TextBox
+    Friend WithEvents lbApellidos As Label
+    Friend WithEvents lbNombres As Label
+    Friend WithEvents lbRut As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents lbSexo As Label
+    Friend WithEvents lbComuna As Label
+    Friend WithEvents lbCiudad As Label
+    Friend WithEvents lbObservacion As Label
+    Friend WithEvents tbxCiudad As TextBox
+    Friend WithEvents tbxObservacion As TextBox
+    Friend WithEvents btnGuardarMySQL As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents cmbComuna As ComboBox
+
+End Class
---
+++ Eva2 Proyecto NET/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private connectionString As String = "Server=localhost;Database=registropersonas;Uid=root;Pwd=;"
+    Private connection As MySqlConnection
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CargarComunas()
+    End Sub
+
+    Private Sub CargarComunas()
+        connection = New MySqlConnection(connectionString)
+        Dim command As MySqlCommand = New MySqlCommand("SELECT NombreComuna FROM comuna", connection)
+
+        Try
+            connection.Open()
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+
+            While reader.Read()
+                cmbComuna.Items.Add(reader("NombreComuna").ToString())
+            End While
+            reader.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar comunas: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    Private Sub btnGuardarMySQL_Click(sender As Object, e As EventArgs) Handles btnGuardarMySQL.Click
+        If ValidarCampos() Then
+            Dim sexo As String = If(RadioButton1.Checked, "Masculino", If(RadioButton2.Checked, "Femenino", "No especifica"))
+            Dim command As MySqlCommand = New MySqlCommand("INSERT INTO Personas (RUT, Nombre, Apellido, Sexo, Comuna, Ciudad, Observacion) VALUES (@RUT, @Nombre, @Apellido, @Sexo, @Comuna, @Ciudad, @Observacion)", connection)
+
+            command.Parameters.AddWithValue("@RUT", tbxRut.Text)
+            command.Parameters.AddWithValue("@Nombre", tbxNombres.Text)
+            command.Parameters.AddWithValue("@Apellido", tbxApellidos.Text)
+            command.Parameters.AddWithValue("@Sexo", sexo)
+            command.Parameters.AddWithValue("@Comuna", cmbComuna.SelectedItem.ToString())
+            command.Parameters.AddWithValue("@Ciudad", tbxCiudad.Text)
+            command.Parameters.AddWithValue("@Observacion", tbxObservacion.Text)
+
+            Try
+                connection.Open()
+                command.ExecuteNonQuery()
+                MessageBox.Show("Datos guardados exitosamente.")
+                LimpiarFormulario()
+                tbxRut.Focus() ' Enfocar en el campo RUT después de limpiar el formulario
+            Catch ex As Exception
+                MessageBox.Show("Error al guardar datos: " & ex.Message)
+            Finally
+                connection.Close()
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim command As MySqlCommand = New MySqlCommand("SELECT * FROM Personas WHERE RUT = @RUT", connection)
+        command.Parameters.AddWithValue("@RUT", tbxRut.Text)
+
+        Try
+            connection.Open()
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+            If reader.Read() Then
+                tbxNombres.Text = reader("Nombre").ToString()
+                tbxApellidos.Text = reader("Apellido").ToString()
+                If reader("Sexo").ToString() = "Masculino" Then
+                    RadioButton1.Checked = True
+                ElseIf reader("Sexo").ToString() = "Femenino" Then
+                    RadioButton2.Checked = True
+                Else
+                    RadioButton3.Checked = True
+                End If
+                cmbComuna.SelectedItem = reader("Comuna").ToString()
+                tbxCiudad.Text = reader("Ciudad").ToString()
+                tbxObservacion.Text = reader("Observacion").ToString() ' Cambiado a "Observacion"
+            Else
+                MessageBox.Show("RUT no encontrado.")
+            End If
+            reader.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error al buscar datos: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    Private Function ValidarCampos() As Boolean
+        If String.IsNullOrWhiteSpace(tbxRut.Text) OrElse
+           String.IsNullOrWhiteSpace(tbxNombres.Text) OrElse
+           String.IsNullOrWhiteSpace(tbxApellidos.Text) OrElse
+           cmbComuna.SelectedIndex = -1 Then
+            MessageBox.Show("Por favor, complete todos los campos obligatorios.")
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub LimpiarFormulario()
+        tbxRut.Clear()
+        tbxNombres.Clear()
+        tbxApellidos.Clear()
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        cmbComuna.SelectedIndex = -1
+        tbxCiudad.Clear()
+        tbxObservacion.Clear()
+        tbxRut.Focus()
+    End Sub
+End Class
